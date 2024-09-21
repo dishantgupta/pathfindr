8e9118a5c26a943f0142ca3b9fb57d6ce4d46911
Dishant Gupta <Dishant@DishantMacAir.local>
2024-09-21 20:34:59 +0530

    cleanup: server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import logging
-import os
 
 from flask import Flask
+
+from integration.amadeus import auth_token_cache
+from url_register import health_url_register, app_url_register
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -14,28 +16,8 @@ logging.basicConfig(
 )
 
 
-def init_app(test_config=None):
-
-    # create and configure the app
+def init_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     return app
 
 
@@ -43,12 +25,10 @@ if __name__ == '__main__':
     app = init_app()
 
     # populate token cache
-    from integration.amadeus import auth_token_cache
     auth_token_cache.create_auth_token_cache()
     logger.debug("generated AMADEUS access token and refresh in cache")
 
     # register health URLs
-    from url_register import health_url_register, app_url_register
     health_url_register.add_url(app)
 
     # register app URLs
